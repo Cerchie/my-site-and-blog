5d144dae2ce68eab929f6a80923ec8e6c85ddf94
Cerchie <luciacerchie@gmail.com>
2023-12-17 08:16:33 -0800

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/global.css';
 
+import { JetBrains_Mono } from 'next/font/google';
+
+const jetbrains = JetBrains_Mono({   
+weight: ['variable'],
+style: ['normal', 'italic'],
+subsets: ['latin'],
+display: 'fallback',});
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+
+  return (
+    <main className={jetbrains.className}>
+  <Component {...pageProps} />
+  </main>
+  )
+
   }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,25 @@
 import Layout from '../../components/layout';
-import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-  
-    return {
-      props: {
-        postData,
-      },
-    };
-  }
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -22,20 +29,11 @@ export async function getStaticPaths() {
   };
 }
 
-
-export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-      </Layout>
-    );
-  }
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
